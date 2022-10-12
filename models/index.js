a1998fdecaf6182aaf3726a9fba10a68bee6a7b6
PivotalSaint <licensedmt@att.net>
2022-10-12 17:17:55 -0500

    its a start to get and post routes

--- models/index.js
+++ models/index.js
@@ -5,9 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_name'
+}
+  )
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_name'
+})
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
