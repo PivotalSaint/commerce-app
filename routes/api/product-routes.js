fa80ba53bac3dbd8f04d74954fc684c2ea8220f8
PivotalSaint <licensedmt@att.net>
2022-10-12 18:28:14 -0500

    trying to launch to heroku

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,15 +6,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  Product.findAll()
-  .then(dbProductData => res.json(dbProductData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'product_name',
+      'price',
+      'stock',
+      'tagIds',
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
+// be sure to include its associated Category and Tag data
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
@@ -37,52 +43,79 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/product', (req, res) => {
+router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      attributes: [
+      'product_name',
+      'price',
+      'stock',
+      'tagIds',
+      ]
     }
   */
- Product.findOne({
-  where: {
-    product: req.body.product
-  }
- })
- .then(dbProductData => {
-  if (!dbProductData){
-  res.status(400).json({ message: 'No product with that tag'});
-  return;
-  }
- })
-
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tag_id
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
       }
-      // if no product tags, just respond
-      res.status(200).json(product);
+      res.json(dbProductData);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
+
+  router.post('/product', (req, res) => {
+
+    Product.findOne({
+      where: {
+        product: req.body.product
+      }
+    })
+
+    Product.create(req.body)
+      .then((product) => {
+        // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+        if (req.body.tagIds.length) {
+          const productTagIdArr = req.body.tagIds.map((tag_id) => {
+            return {
+              product_id: product.id,
+              tag_id,
+            };
+          });
+          return ProductTag.bulkCreate(productTagIdArr);
+        }
+        // if no product tags, just respond
+        res.status(200).json(product);
+      })
+      .then((productTagIds) => res.status(200).json(productTagIds))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 });
 
 // update product
